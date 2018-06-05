f6f2aab41d406a932771292b0d9495de1130e22e
Madoka09 <shippuuga.zero@gmail.com>
2018-06-05 08:34:09 -0500

    Añadida Nueva practica

---
+++ verbs/verbs.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(request,response){
+    response.send('Usaste GET');
+});
+
+app.post('/',function(request,response){
+    response.send('Usaste POST');
+});
+
+app.put('/',function(request,response){
+    response.send('Usaste PUT');
+});
+
+app.delete('/',function(request,response){
+    response.send('Usaste DELETE');
+});
+
+app.listen(3000);
